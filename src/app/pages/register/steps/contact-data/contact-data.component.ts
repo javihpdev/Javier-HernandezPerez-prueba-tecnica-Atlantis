c71e001af513701ac480053e66261b9d7dba4929
javier <javierhpdev@gmail.com>
2026-06-25 18:23:34 +0100

    implementado el step de contacto

--- src/app/pages/register/steps/contact-data/contact-data.component.ts
+++ src/app/pages/register/steps/contact-data/contact-data.component.ts
@@ -1,12 +1,37 @@
-import { Component } from "@angular/core";
-
+import { Component, inject, input, output } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterService } from '../../../../services/register/register.service';
+import { InputComponent } from '../../../../components/atoms/input/input.component';
+import { ButtonComponent } from '../../../../components/atoms/button/button.component';
 
 @Component({
-  standalone:true,
-  selector:'ContactDataComponent',
-  templateUrl:'./contact-data.component.html'
+  selector: 'app-contact-data',
+  imports: [ReactiveFormsModule, InputComponent, ButtonComponent],
+  templateUrl: './contact-data.component.html',
+  styleUrl: './contact-data.component.scss'
 })
-
 export class ContactDataComponent {
+  readonly step = input<number>(2);
+  readonly totalSteps = input<number>(3);
 
+  readonly next = output<void>();
+  readonly previous = output<void>();
+
+  private readonly registerService = inject(RegisterService);
+
+  get form() {
+    return this.registerService.contactDataForm;
+  }
+
+  onSubmit(): void {
+    this.registerService.triggerValidation(this.form);
+    if (this.form.valid) {
+      this.next.emit();
+    }
+  }
+
+  goBack(): void {
+    this.previous.emit();
+  }
 }
+ 
