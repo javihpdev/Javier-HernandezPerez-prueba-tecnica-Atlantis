000bed6caa78cf0a560f1011fb924d7a1bb85213
javier <javierhpdev@gmail.com>
2026-06-26 12:05:18 +0100

    implementado nuevo layout principal con header y un footer sticky

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,18 +1,14 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from "@angular/router";
-import {TranslateModule} from "@ngx-translate/core";
-import {BreadcrumbComponent} from "../../components/breadcrumb/breadcrumb.component";
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { BreadcrumbComponent } from '../../components/breadcrumb/breadcrumb.component';
+
 @Component({
-    selector: 'app-main',
-    imports: [
-        RouterOutlet,
-        TranslateModule,
-        BreadcrumbComponent,
-    ],
-    templateUrl: './main.component.html',
-    styleUrls: ['./main.component.scss']
+  selector: 'app-main',
+  imports: [RouterOutlet, RouterLink, BreadcrumbComponent],
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
 })
-export class MainComponent{
+export class MainComponent {
   // private _authService: AuthService = inject(AuthService);
   // private _rolesService: RolesService = inject(RolesService);
 
